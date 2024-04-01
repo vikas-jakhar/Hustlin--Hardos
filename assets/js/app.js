0eaf3074c3ce43ca0ad108d68fecc923eef6826b
vikas-jakhar <vikashjakhar2004@gmail.com>
2024-04-01 12:50:35 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,31 @@
+const menuicon = document.querySelector("#hamburger-icon");
+const menulist = document.querySelector(".menulist");
+
+menuicon.addEventListener("click", function () {
+    menulist.classList.toggle("showlist");
+    menuicon.classList.toggle("fixed1")
+});
+
+$('.slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
